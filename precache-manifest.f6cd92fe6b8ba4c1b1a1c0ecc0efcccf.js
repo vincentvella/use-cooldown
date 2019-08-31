73d248c31b8d9b35b95cad7435661969502be444
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-31 18:47:06 +0000

    Deploy vincentvella/use-cooldown to github.com/vincentvella/use-cooldown.git:gh-pages

---
+++ precache-manifest.f6cd92fe6b8ba4c1b1a1c0ecc0efcccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce215dbd4ca0210606dfadd76711ce1",
+    "url": "/use-cooldown/index.html"
+  },
+  {
+    "revision": "aff79e444bff93082710",
+    "url": "/use-cooldown/static/css/main.a69a4fa5.chunk.css"
+  },
+  {
+    "revision": "83fb69b5246895f262e0",
+    "url": "/use-cooldown/static/js/2.ab02646e.chunk.js"
+  },
+  {
+    "revision": "aff79e444bff93082710",
+    "url": "/use-cooldown/static/js/main.b4ac5be3.chunk.js"
+  },
+  {
+    "revision": "e6587c5290855b84aceb",
+    "url": "/use-cooldown/static/js/runtime~main.7fdd76f2.js"
+  }
+]);
